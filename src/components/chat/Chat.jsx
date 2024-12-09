2da01b5f0f0941479070ecc80b20ad2e52bbe711
MeenuKumari17 <m1798kumari@gmail.com>
2024-12-09 16:27:46 +0530

    added layout

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,8 +1,95 @@
-import "./chat.css"
+import { useState } from "react";
+import "./chat.css";
+import EmojiPicker from "emoji-picker-react";
 
 const Chat = () => {
+  const [open, setOpen] = useState(false)
+  const [text, setText] = useState("")
+  const handleEmoji = e => {
+    setText((prev) => prev + e.emoji )
+    setOpen(false)
+  }
   return (
-    <div>Chat</div>
+    <div className="chat">
+        <div className="top">
+          <div className="user">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+              <span>John Deo</span>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+          </div>
+          <div className="icons">
+            <img src="./phone.png" alt="" />
+            <img src="./video.png" alt="" />
+            <img src="./info.png" alt="" />
+          </div>
+        </div>
+        <div className="center">
+          <div className="message">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+              <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+              Ut enim ad minim veniam.
+              </p>
+              <span>
+                1 min ago
+              </span>
+            </div>
+          </div>
+          <div className="message own">
+            <div className="texts">
+              <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+              Ut enim ad minim veniam.
+              </p>
+              <span>
+                1 min ago
+              </span>
+            </div>
+          </div>
+          <div className="message">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+              <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+              Ut enim ad minim veniam.
+              </p>
+              <span>
+                1 min ago
+              </span>
+            </div>
+          </div>
+          <div className="message own">
+            <div className="texts">
+              <img src="https://fastly.picsum.photos/id/21/3008/2008.jpg?hmac=T8DSVNvP-QldCew7WD4jj_S3mWwxZPqdF0CNPksSko4" alt="" />
+              <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+              Ut enim ad minim veniam.
+              </p>
+              <span>
+                1 min ago
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="icons">
+            <img src="./img.png" alt="" />
+            <img src="./camera.png" alt="" />
+            <img src="./mic.png" alt="" />
+          </div>
+          <input type="text" value={text} placeholder="Type a message..." onChange={e=> setText(e.target.value)}/>
+          <div className="emoji">
+            <img src="./emoji.png" alt="" onClick={() => setOpen((prev) => !prev)}/>
+            <div className="picker">
+            <EmojiPicker open={open} onEmojiClick={handleEmoji}/>
+            </div>
+          </div>
+          <button className="sendButton">Send</button>
+        </div>
+    </div>
   )
 }
 
--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -2,7 +2,49 @@ import "./detail.css"
 
 const Detail = () => {
   return (
-    <div>Detail</div>
+    <div className="detail">
+      <div className="user">
+        <img src="'./avatar.png" alt="" />
+        <h2>John Deo</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Setting</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy % help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+              <img src="https://fastly.picsum.photos/id/21/3008/2008.jpg?hmac=T8DSVNvP-QldCew7WD4jj_S3mWwxZPqdF0CNPksSko4" alt="" />
+              <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <button>Block User</button>
+      </div>
+    </div>
   )
 }
 
