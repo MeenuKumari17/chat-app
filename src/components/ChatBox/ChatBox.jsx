b1875349ee88218107fe036abff06c46bcc5705f
MeenuKumari17 <m1798kumari@gmail.com>
2025-01-16 20:14:16 +0530

    added ui for chat box

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -7,10 +7,40 @@ const ChatBox = () => {
     <div className='chat-box'>
       <div className="chat-user">
         <img src={assets.profile_img} alt="" />
-        <p>John Deo <img src={assets.green_dot} alt="" /></p>
+        <p>John Deo <img src={assets.green_dot} className='dot' alt="" /></p>
         <img src={assets.help_icon} className='help' alt="" />
       </div>
-      
+      <div className='chat-msg'>
+        <div className="s-msg">
+          <p className="msg">Lorem Ipsum is simply dummy text of the printing.</p>
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p>2:30 PM</p>
+          </div>
+        </div>
+        <div className="s-msg">
+          <img className='msg-img' src={assets.pic1} alt="" />
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p>2:30 PM</p>
+          </div>
+        </div>
+        <div className="r-msg">
+          <p className="msg">Lorem Ipsum is simply dummy text of the printing.</p>
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p>2:30 PM</p>
+          </div>
+        </div>
+      </div>
+      <div className="chat-input">
+        <input type="text" placeholder='Send a message...' />
+        <input type="file" accept='image/png, image/jpeg' hidden id="image" />
+        <label htmlFor="image">
+          <img src={assets.gallery_icon} alt="" />
+        </label>
+        <img src={assets.send_button} alt="" />
+      </div>
     </div>
   )
 }
